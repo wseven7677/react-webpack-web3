7d881c84cd7a506c862c2b4d11b40d9d1cd50bdb
localWseven <caoyu_92@126.com>
2017-09-09 22:29:53 +0800

    浏览器兼容性调整+个人介绍修改

--- src/scripts/sub/me.jsx
+++ src/scripts/sub/me.jsx
@@ -9,7 +9,7 @@ class Me extends React.Component {
 			<img src={authorPicSrc} />
 			<div className="meDesc">
 				<span>本站作者</span>
-				<span>一个做事三周热度的人。加里敦联盟资深会员。欢迎去看我的github和我的漫画。没有彩蛋。</span>
+				<span>不爱写代码的设计狮不是好通信研究生。欢迎去看我的github<div className="div-del">和我的漫画</div>。</span>
 			</div>
 			<div className="gradientBlock"></div>
 		</div>;
